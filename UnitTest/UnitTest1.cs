e7e48ab6ab0f2a23c8b4a4375544c1771016500b
Elky (SeongHoon Kim) <elky84@gmail.com>
2022-01-15 20:49:55 +0900

    added UnitTest

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    public class TimeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
